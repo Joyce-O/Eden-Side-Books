006032a726c1b0cb3715e9d54b7bfcd2c870bd6d
user <user@users-MacBook-Pro.local>
2019-10-25 08:28:50 +0100

    Add react to replace vanila Js

--- Tinker/M3/index.js
+++ Tinker/M3/index.js
@@ -1,7 +1,14 @@
 const rootElement = document.getElementById('App');
+// VANILA JS
+// const myElement = document.createElement('h1');
+// myElement.className = 'orange';
+// myElement.innerText = 'Hello From JoyceLand';
 
-const myElement = document.createElement('hi');
-myElement.className = 'orange';
-myElement.innerText = 'Hello From JoyceLand';
+// INTRODUCE REACT
+const myReactElement = React.createElement('h1', {className: 'orange'}, 'Hello From JoyceLand and React');
 
-rootElement.appendChild(myElement);
+// rootElement.appendChild(myElement); // Useful for V JS
+
+ReactDOM.render(
+    myReactElement, document.getElementById('App')
+);
