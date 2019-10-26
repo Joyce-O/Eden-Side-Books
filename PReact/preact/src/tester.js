68a8e49aac108eab753a12e18bdd405f24e14ab6
user <user@users-MacBook-Pro.local>
2019-10-26 17:39:21 +0100

    Move the status and history to Game component

---
+++ PReact/preact/src/tester.js
@@ -0,0 +1,10 @@
+// const myArr = Array(7).fill(8);
+// const newArr = myArr.slice();
+// console.log(newArr);
+
+const fun = (myArr) => {
+    const myArr = Array(7).fill(8);
+    const newArr = myArr.slice();
+    
+    console.log(newArr);
+}
